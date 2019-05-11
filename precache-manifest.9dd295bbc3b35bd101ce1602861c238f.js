95e2fad0c1173c4d81c4f8f9dc2aa1655fb5795a
Srikanth khandavally <khandavallysrikanth@yahoo.com>
2019-05-11 21:43:52 +0800

    Updates

---
+++ precache-manifest.9dd295bbc3b35bd101ce1602861c238f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a763140b1d001fbe034ec6c257447fa5",
+    "url": "/React-web-app/static/media/intro_cream_of_crop.a763140b.jpg"
+  },
+  {
+    "revision": "8c44fe326c6656a987d7",
+    "url": "/React-web-app/static/js/runtime~main.8c44fe32.js"
+  },
+  {
+    "revision": "6aed2f9707d95ac4281e",
+    "url": "/React-web-app/static/js/main.6aed2f97.chunk.js"
+  },
+  {
+    "revision": "7c98983e474f39ad4bf9",
+    "url": "/React-web-app/static/js/1.7c98983e.chunk.js"
+  },
+  {
+    "revision": "6aed2f9707d95ac4281e",
+    "url": "/React-web-app/static/css/main.fc318399.chunk.css"
+  },
+  {
+    "revision": "4641dd8ebfbe79d90789ae97106586f3",
+    "url": "/React-web-app/index.html"
+  }
+];
